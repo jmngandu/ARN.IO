3ad9356dde464cb1c5e6c05a23458d558ca88d9a
jmngandu <jamesmngandu@gmail.com>
2024-03-10 19:46:49 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 function App() {
  
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
     <Header/>
-     <h1>Hello dad and mum</h1>
+    <Hero />
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Hero(props){
+    return(
+        <>
+        <div className="hero-section">
+            <h1 className="hero-title">ARN.IO</h1>
+            <div className="hero-pages">
+                <div className="hero-single">
+                <h1>plan your reading</h1>
+                <div className="inner-page">
+                <h2>schedule</h2>
+                <p>Worried you never stick to reading ?</p>
+                <p>Track your progress efficently </p>
+                </div>
+                </div>
+                <hr/>
+            </div>            
+        </div>
+        </>
+    )
+}
