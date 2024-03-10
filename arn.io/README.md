3653426d2a99699f7d774e54cf5f2e0c9d3530c2
jmngandu <jamesmngandu@gmail.com>
2024-03-10 17:59:38 +0300

    commit

---
+++ arn.io/README.md
@@ -0,0 +1,2 @@
+# ARN.IO
+> ARN.IO is  a learning app that helps the learners to track their learning journey
---
+++ arn.io/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>Hello dad and mum</h1>
+    </>
+  )
+}
+
+export default App
