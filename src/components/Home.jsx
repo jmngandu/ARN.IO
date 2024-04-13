ef136ae01c17d8d49a8976f4b45637c191ff05bc
jmngandu <jamesmngandu@gmail.com>
2024-04-13 12:37:40 +0300

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,23 +4,19 @@ import Hero from "./Hero";
 import Title from "./Title";
 import heroData from "./heroData";
 
-
-export default function Home(){
-    const heroDataset = heroData.map(main=>{
-        return(
-          <>    
-           <Hero
-            key={main.id}
-            main={main}/>
-          </>
-        )
-      })
-      return (
-        <>
-        <Header/>
-        <Title />
-        <div className="heroMain">{heroDataset} </div>
-        </>
-      )
-    }
-    
+export default function Home() {
+  const heroDataset = heroData.map((main) => {
+    return (
+      <>
+        <Hero key={main.id} main={main} />
+      </>
+    );
+  });
+  return (
+    <>
+      <Header />
+      <Title />
+      <div className="heroMain">{heroDataset} </div>
+    </>
+  );
+}
