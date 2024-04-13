4ff8e321b1348600a2277ffcdd72ca9e098e6dd3
jmngandu <jamesmngandu@gmail.com>
2024-04-13 12:37:19 +0300

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 
-export default function Hero(props){
- 
-    return(
-        <>
-        <div className="hero-section"> 
-            <div className="hero-pages">
-            <hr/>
-                <div className="hero-single">
-                    <h1>{props.main.title}</h1>
-                    <div className="inner-page">
-                        <h2>{props.main.source}</h2>
-                        <p>{props.main.firstDescription}</p>
-                        <p>{props.main.secondDescription} </p>
-                    </div>
-                </div>
-                <hr/>
-            </div>          
+export default function Hero(props) {
+  return (
+    <>
+      <div className="hero-section">
+        <div className="hero-pages">
+          <hr />
+          <div className="hero-single">
+            <h1>{props.main.title}</h1>
+            <div className="inner-page">
+              <h2>{props.main.source}</h2>
+              <p>{props.main.firstDescription}</p>
+              <p>{props.main.secondDescription} </p>
+            </div>
+          </div>
+          <hr />
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
