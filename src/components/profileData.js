fa5c6dd742c72a7f8cdbdffd33bdeb383f2fe7bc
jmngandu <jamesmngandu@gmail.com>
2024-04-13 12:37:58 +0300

    commit

--- src/components/profileData.js
+++ src/components/profileData.js
@@ -1,1 +1,7 @@
-export default [{ id:1, img:"login.jpg", text:"The boy who carried wolf" }, { id:2, img:"login.jpg", text:"Foundation of chemistry" }, { id:3, img:"login.jpg", text:"BMS notes" }, { id:4, img:"login.jpg", text:"The rosicrucian book" }, { id:5, img:"login.jpg", text:"Foundations of physics" } ]
+export default [
+  { id: 1, img: "login.jpg", text: "The boy who carried wolf" },
+  { id: 2, img: "login.jpg", text: "Foundation of chemistry" },
+  { id: 3, img: "login.jpg", text: "BMS notes" },
+  { id: 4, img: "login.jpg", text: "The rosicrucian book" },
+  { id: 5, img: "login.jpg", text: "Foundations of physics" },
+];
