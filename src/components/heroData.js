77c43501658da54f28cfefca62a47ffda3f053ba
jmngandu <jamesmngandu@gmail.com>
2024-04-13 12:37:30 +0300

    commit

--- src/components/heroData.js
+++ src/components/heroData.js
@@ -1,1 +1,23 @@
-export default[{id:1,title:"Plan your reading",source:"Schedule", firstDescription:"Worried you never stick to reading ?", secondDescription:"Track your progress efficently" }, { id:2,title:"Access reading", source:"Resources", firstDescription:"Get a resource to a vast library of reading resources", secondDescription:"From books to pdfs" }, {id:3, title:"Upload your own", source:"Documents", firstDescription:"Want your own document ?, no worries", secondDescription:"Upload yours easily" } ]
+export default [
+  {
+    id: 1,
+    title: "Plan your reading",
+    source: "Schedule",
+    firstDescription: "Worried you never stick to reading ?",
+    secondDescription: "Track your progress efficently",
+  },
+  {
+    id: 2,
+    title: "Access reading",
+    source: "Resources",
+    firstDescription: "Get a resource to a vast library of reading resources",
+    secondDescription: "From books to pdfs",
+  },
+  {
+    id: 3,
+    title: "Upload your own",
+    source: "Documents",
+    firstDescription: "Want your own document ?, no worries",
+    secondDescription: "Upload yours easily",
+  },
+];
