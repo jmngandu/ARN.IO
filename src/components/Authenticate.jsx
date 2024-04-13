bc5212e2642cec61d34e0a8a73248d61c4ec1d2d
jmngandu <jamesmngandu@gmail.com>
2024-04-13 12:36:46 +0300

    commit

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,25 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Authenticate(){
-    return(
-        <>
-        <div className="authenticate">
-            <img src="src/assets/images/login.jpg" id="login-img"/>
-            <form className="form-section">
-            <input type="email" placeholder="Gmail account" name="email"className="input" id="input-email"/>
-            <input type="password" placeholder="password" name="password" className="input"id="input-password"/>
+export default function Authenticate() {
+  return (
+    <>
+      <div className="authenticate">
+        <img src="src/assets/images/login.jpg" id="login-img" />
+        <form className="form-section">
+          <input
+            type="email"
+            placeholder="Gmail account"
+            name="email"
+            className="input"
+            id="input-email"
+          />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            className="input"
+            id="input-password"
+          />
 
-            <button id="login-btn"><Link to="/Dashboard" className="linked">login</Link></button>
-           
-            <div className="lower-input">
-                <p>Don't have an account?</p>
-                <p><Link to="/SignUp" className="signup-con">signup</Link></p>
-            </div>
-            </form>
-        
-        </div>
-        
-        </>
-    )
+          <button id="login-btn">
+            <Link to="/Dashboard" className="linked">
+              login
+            </Link>
+          </button>
+
+          <div className="lower-input">
+            <p>Don't have an account?</p>
+            <p>
+              <Link to="/SignUp" className="signup-con">
+                signup
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
