c22f49e78cd3810a850a1bdd06e4bef1eb8bae1f
jmngandu <jamesmngandu@gmail.com>
2024-10-23 20:39:24 +0300

    arn.io

--- frontend/src/components/profileData.js
+++ frontend/src/components/profileData.js
@@ -5,7 +5,11 @@ export default [
     text: "The boy who carried wolf",
   },
   { id: 2, img: "login.jpg", text: "Foundation of chemistry" },
-  { id: 3, img: "login.jpg", text: "BMS notes" },
+  {
+    id: 3,
+    img: "https://res.cloudinary.com/dicfffpsh/image/upload/v1729704806/ARN/book_qcojey.jpg",
+    text: "BMS notes",
+  },
   { id: 4, img: "login.jpg", text: "The rosicrucian book" },
   { id: 5, img: "login.jpg", text: "Foundations of physics" },
 ];
